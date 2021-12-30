fee89402facbd4b1ba7aa759db784eb4953cd505
Chuan <nauhcnay@gmail.com>
2021-12-30 03:00:31 -0500

    client and server ready

--- run_client_test.py
+++ run_client_test.py
@@ -6,60 +6,91 @@ import json
 import argparse
 import configparser
 
-url = "http://164.90.158.133:8080"
-to_speech = "/to_speech"
+url_root = "http://164.90.158.133:8080"
+url_get_speech = "/to_speech"
+url_get_speaker = "/get_speaker"
+url_get_audios = "/is_ready"
+path_menu = './Menu.ini'
+path_st = './Sentences.ini'
+path_cus = './Customer.ini'
 
-
-# data =  {
-#             "order": ["00", "10", "20", "30"],
-#             "speaker": "p225"
-#         }
-
-
-speakers = {'ED\n': 0, 'p225': 1, 'p226': 2, 'p227': 3, 'p228': 4, 'p229': 5, 
-    'p230': 6, 'p231': 7, 'p232': 8, 'p233': 9, 'p234': 10, 'p236': 11, 'p237': 12, 
-    'p238': 13, 'p239': 14, 'p240': 15, 'p241': 16, 'p243': 17, 'p244': 18, 'p245': 19, 
-    'p246': 20, 'p247': 21, 'p248': 22, 'p249': 23, 'p250': 24, 'p251': 25, 'p252': 26, 
-    'p253': 27, 'p254': 28, 'p255': 29, 'p256': 30, 'p257': 31, 'p258': 32, 'p259': 33, 
-    'p260': 34, 'p261': 35, 'p262': 36, 'p263': 37, 'p264': 38, 'p265': 39, 'p266': 40, 
-    'p267': 41, 'p268': 42, 'p269': 43, 'p270': 44, 'p271': 45, 'p272': 46, 'p273': 47, 
-    'p274': 48, 'p275': 49, 'p276': 50, 'p277': 51, 'p278': 52, 'p279': 53, 'p280': 54, 
-    'p281': 55, 'p282': 56, 'p283': 57, 'p284': 58, 'p285': 59, 'p286': 60, 'p287': 61, 
-    'p288': 62, 'p292': 63, 'p293': 64, 'p294': 65, 'p295': 66, 'p297': 67, 'p298': 68, 
-    'p299': 69, 'p300': 70, 'p301': 71, 'p302': 72, 'p303': 73, 'p304': 74, 'p305': 75, 
-    'p306': 76, 'p307': 77, 'p308': 78, 'p310': 79, 'p311': 80, 'p312': 81, 'p313': 82, 
-    'p314': 83, 'p316': 84, 'p317': 85, 'p318': 86, 'p323': 87, 'p326': 88, 'p329': 89, 
-    'p330': 90, 'p333': 91, 'p334': 92, 'p335': 93, 'p336': 94, 'p339': 95, 'p340': 96, 
-    'p341': 97, 'p343': 98, 'p345': 99, 'p347': 100, 'p351': 101, 'p360': 102, 'p361': 103, 
-    'p362': 104, 'p363': 105, 'p364': 106, 'p374': 107, 'p376': 108}
-    
-# read the ini file
+# read the ini file to string
 def read_config(path_to_ini):
-    config = configparser.ConfigParser()
-    config.read(path_to_ini)
+    with open(path_to_ini, "r") as f:
+        config = f.read()
     return config
 
-# send 
-def
+# tell if the audio file is ready, since generating all 
+# audio files will probably takes a long time and it will time out
+# the client.
+def is_ready():
+    pass
 
+# get all speech audio files and unzip them into './audios/'
 def get_speeches(menu, customers, sentences):
-    data = []
-    result = requests.post(url+to_speech, json = json.dumps(data))
+    req = {}
+    req["menu"] = read_config(menu)
+    req["customers"] = read_config(customers)
+    req["sentences"] = read_config(sentences)
+    result = requests.post(url_root+url_get_speech, json=req)
+    # download the file when it is ready
+    # or maybe we can print a progress bar here
+    print("Log:\t%s"%result.text)
+    if result.status_code == 200:
+        while True:
+            result = requests.get(url_root+url_get_audios, stream=True)
+            if result.status_code == 200:
+                with open("./speech_audios.zip", "wb") as f:
+                    f.write(result.content)
+                print("log:\tsaved all files to ./speech_audios.zip")
+                break
+
+# get and save speakers into ./speakers.ini
+def get_speakers(path_to_speaker):
+    result = requests.get(url_root+url_get_speaker)
+    if result.status_code == 200:
+        speakers = result.json()
+        if type(speakers) == str:
+            speakers = speakers.strip("[").strip("]").split(",")
+        config_speakers = configparser.ConfigParser()
+        dict_speakers = {}
+        # import pdb
+        # pdb.set_trace()
+        for idx, spk in enumerate(speakers):
+            dict_speakers["%03d"%idx] = spk.strip(" ").strip("\"")
+        config_speakers["speakers"] = dict_speakers
+        with open(path_to_speaker, "w") as f:
+            config_speakers.write(f)
+        print("Log:\tspeakers are saved to %s"%path_to_speaker)
+    else:
+        print("Error:\tfailed to get correct speak info")
+
+# verify if all needed audio files are ready
+def verify_speeches(menu, customers, sentences):
+    pass
 
 if __name__ == "__main__":
     # parse cmd lines
     parser = argparse.ArgumentParser()
-    parse.add_argument('--to_speech', type=str, 
+    parser.add_argument('--speech', action='store_true',
         help="get all speech audios in a zip file")
-    parse.add_argument('--get_speaker', type=str, 
+    parser.add_argument('--speaker', action='store_true',
         help="get all speaker id and put them into ./speaker.ini")
-
-    parser.add_argument("--menu", type=str, defualt='./menu.ini',
-        help="the path to menu config file, defualt is ./menu.ini")
-    parser.add_argument("--customers", type=str, defualt='./Customer.ini',
-        help="the path to customer config file, defualt is ./Customer.ini")
-    parser.add_argument("--sentences", type=str, defualt='./sentences.ini',
-        help="the path to sentence template file, defualt is ./sentences.ini")
+    parser.add_argument("--menu", type=str, default='./Menu.ini',
+        help="the path to menu config file, default is ./Menu.ini")
+    parser.add_argument("--customers", type=str, default='./Customer.ini',
+        help="the path to customer config file, default is ./Customer.ini")
+    parser.add_argument("--sentences", type=str, default='./Sentences.ini',
+        help="the path to sentence template file, default is ./Sentences.ini")
+    parser.add_argument("--speakers", type=str, default='./Speakers.ini',
+        help="the path to save speaker keys, default is ./Speakers.ini")
     args = parser.parse_args()
 
     # send request and get result
+    if args.speech:
+        get_speeches(args.menu, args.customers, args.sentences)
+    elif args.speaker:
+        get_speakers(args.speakers)
+    else:
+        print("Log:\tno commands found, please indicate one operation you want")
+        parser.print_help()
--- run_server.py
+++ run_server.py
@@ -3,12 +3,16 @@ import os
 import base64
 import urllib.parse
 import configparser
+import copy
+import shutil
+from multiprocessing import Process
 from random import choice
 from aiohttp import web
 
 '''
 text and speech generation
 '''
+ps = []
 speakers = {'ED\n': 0, 'p225': 1, 'p226': 2, 'p227': 3, 'p228': 4, 'p229': 5, 
     'p230': 6, 'p231': 7, 'p232': 8, 'p233': 9, 'p234': 10, 'p236': 11, 'p237': 12, 
     'p238': 13, 'p239': 14, 'p240': 15, 'p241': 16, 'p243': 17, 'p244': 18, 'p245': 19, 
@@ -41,14 +45,15 @@ def to_base64():
     speech = base64.encodebytes(speech).decode("utf-8")
     return speech
 
-def text_to_speech(text, speaker):
+def text_to_speech(text, speaker, outpath):
     cmd = "tts --model_name \"tts_models/en/vctk/vits\" --text "
-    cmd += text
-    cmd += " --speaker_idx %s "%speaker
-    cmd += "--use_cuda true"
+    cmd += "\""+text+"\""
+    cmd += " --speaker_idx %s"%speaker
+    cmd += " --use_cuda true"
+    cmd += " --out_path ./audio_output/" + outpath
     os.system(cmd)
 
-def gen_text(request):
+def gen_text_old(request):
     # try to add more useless words to make the sentense more realistic
     order = request["order"]
     speaker = request["speaker"][0]
@@ -69,6 +74,111 @@ def gen_text(request):
     st = "\"" + st + "\""
     return st, speaker
 
+def order_to_text(order, menu):
+    order_real_st = []
+    order_real = {}
+    for key in order:
+        order_real_st.append(menu[key])
+        order_real[key] = menu[key]
+    if len(order_real_st) > 1:
+        order_real_st[-1] = "and " + order_real_st[-1]
+    return ", ".join(order_real_st), order_real
+
+def gen_text(customers, menu, sentences):
+    # return the dict of file name and text content and speaker list
+    texts = {}
+    for c_key in customers:
+        incident_key = list(sentences.keys())
+        incident_key.sort()
+        speaker = customers[c_key]["speaker"]
+        order_st, order = order_to_text(customers[c_key]["meal_order"], menu)
+        extra_order_st, extra_order = order_to_text(customers[c_key]["extra_order"], menu)
+        # ReadyToOrder
+        for st_key in sentences[incident_key[0]]:
+            name = c_key + incident_key[0] + st_key + ".wav"
+            texts[name] = [sentences[incident_key[0]][st_key].replace("_", order_st), speaker]
+
+        # WantFoodIncident
+        for st_key in sentences[incident_key[1]]:
+            for food_key in order:
+                name = c_key + incident_key[1] + st_key + food_key + ".wav"
+                texts[name] = [sentences[incident_key[1]][st_key].replace("_", order[food_key]), speaker]
+
+        # CheckOutIncident
+        for st_key in sentences[incident_key[2]]:
+            name = c_key + incident_key[2] + st_key + ".wav"
+            texts[name] = [sentences[incident_key[2]][st_key], speaker]
+
+        # CreditCardIncident
+        for st_key in sentences[incident_key[3]]:
+            name = c_key + incident_key[3] + st_key + ".wav"
+            texts[name] = [sentences[incident_key[3]][st_key], speaker]
+
+        # OrderMoreIncident
+        for st_key in sentences[incident_key[4]]:
+            name = c_key + incident_key[4] + st_key + ".wav"
+            texts[name] = [sentences[incident_key[4]][st_key].replace("_", extra_order_st), speaker]
+
+        # DropDrinkIncident
+        for st_key in sentences[incident_key[5]]:
+            for food_key in order:
+                if "m3" not in food_key: continue
+                name = c_key + incident_key[5] + st_key + food_key + ".wav"
+                texts[name] = [sentences[incident_key[5]][st_key].replace("_", order[food_key]), speaker]
+
+        # FoodReplacementIncident
+        for st_key in sentences[incident_key[6]]:
+            for food_key in order:
+                if "m3" in food_key: continue
+                name = c_key + incident_key[6] + st_key + food_key + ".wav"
+                texts[name] = [sentences[incident_key[6]][st_key].replace("_", order[food_key]), speaker]
+    return texts
+
+def read_customer(customers_text):
+    # return customer list, order list, extra order list, incident list
+    customers_ini = configparser.ConfigParser()
+    customers_ini.read_string(customers_text)
+    customers = {}
+    for sec in customers_ini:
+        if sec.lower() == "default": continue
+        if "-Customer" in sec:
+            g, c = sec.split("-")
+            g = "g%d"%int(g.strip("Group"))
+            c = "c%d"%int(c.strip("Customer"))
+            name = g + c
+            customers[name] = {}
+            customers[name]["speaker"] = customers_ini[sec]["speaker"]
+            customers[name]["meal_order"] = customers_ini[sec]["mealOrder"].split(",")
+            customers[name]["extra_order"] = customers_ini[sec]["extraOrder"].split(",")
+            customers[name]["incidents"] = customers_ini[sec]["IncidentOrder"].split(",")
+    return customers
+
+def read_sentences(sentences_text):
+    # return sentence template list per incidents
+    sentences_ini = configparser.ConfigParser()
+    sentences_ini.read_string(sentences_text)
+    st_template = {}
+    idx = 1
+    for sec in sentences_ini:
+        if sec.lower() == "default": continue
+        name = "i%d"%idx
+        idx += 1
+        st_template[name] = {}
+        for st_key in sentences_ini[sec].keys():
+            st_template[name][st_key] = sentences_ini[sec][st_key]
+    return st_template        
+
+def read_menu(menu_text):
+    # returns the dict mapping from key to food name
+    menu_ini = configparser.ConfigParser()
+    menu_ini.read_string(menu_text)
+    menu = {}
+    for sec in menu_ini:
+        if sec.lower() == "default": continue
+        for key in menu_ini[sec]:
+            menu[key] = menu_ini[sec][key]
+    return menu
+
 '''
 HTTP server
 '''
@@ -80,18 +190,18 @@ async def welcome(request):
     return web.Response(text="The TTS server for VR restaruant is running!")
 
 @routes.get("/get_menu")
-async def welcome(request):
+async def get_menu(request):
     with open("foods.json", 'r') as f:
         ans = json.load(f)
     return web.json_response(ans)
 
 @routes.get("/get_speaker")
-async def welcome(request):
+async def get_speaker(request):
     ans = json.dumps(list(speakers.keys()))
     return web.json_response(ans)
 
 @routes.post("/set_menu")
-async def welcome(request):
+async def set_menu(request):
     req = await request.json()
     req = json.loads(req)
     print("log:\tget new menu \n%s:"%str(req))
@@ -105,6 +215,7 @@ async def welcome(request):
     "speaker": "p225"
 }
 '''
+'''
 @routes.post("/to_speech")
 async def welcome(request):
     req = await request.text()
@@ -112,23 +223,61 @@ async def welcome(request):
     print("log:\tget message %s"%req)
     req = json.loads(req)
     print("Log:\tget request %s"%str(req))
-    text, speaker = gen_text(req)
+    text, speaker = gen_text_old(req)
     text_to_speech(text, speaker)
     # maybe don't encode it would be better
-    '''
-    data = to_base64()
-    result = {}
-    result["data"] = data
-    return web.json_response(result)
-    '''
+    # data = to_base64()
+    # result = {}
+    # result["data"] = data
+    # return web.json_response(result)
     with open("tts_output.wav", 'rb') as output:
         speech = output.read()
 
     return web.Response(body=speech)
+'''
+
+@routes.post("/to_speech")
+async def to_speech(request):
+    req = await request.json()
+    if len(ps) == 0:
+        sentences = read_sentences(req["sentences"])
+        menu = read_menu(req["menu"])
+        customers = read_customer(req["customers"])
+        texts = gen_text(customers, menu, sentences)
+        # clean up all exsiting files
+        print("Log:\tclean up output folder")
+        for f in os.listdir("./audio_output"):
+            os.remove(os.path.join("./audio_output", f))
+        p = create_job(to_speech_multi_proc, texts)
+        ps.append(p)
+        return web.Response(text="processing...")
+    else:
+        return web.Response(status=404, text="server busy, please try again later")
 
 @routes.get("/talk")
-async def welcome(request):
+async def talk(request):
     return web.Response(text="under construction")
+
+@routes.get("/is_ready")
+async def is_ready(request):
+    if os.path.exists("./audios.zip"):
+        ps.pop()
+        with open("./audios.zip", "rb") as f:
+            audios = f.read()
+        return web.Response(status=200, body=audios)
+    else:
+        return web.Response(status=404, text="busy")
+
+def to_speech_multi_proc(texts):
+    for file_name in texts: 
+        text, speaker = texts[file_name]
+        text_to_speech(text, speaker, file_name)
+    shutil.make_archive("./audios.zip", 'zip', "./audio_output/")
+
+def create_job(target, *args):
+    p = Process(target=target, args=args)
+    p.start()
+    return p
 
 def main():
     app = web.Application()
