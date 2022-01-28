db62c30ba9c18ffb91393d3e5c0b135de2b7511c
YQZ530 <yongqizhang530@gmail.com>
2022-01-28 17:31:15 -0500

    update misc

--- run_client_test.py
+++ run_client_test.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 # python run_client_test.py -c  -customers ./../Customer.ini -o ./../../Assets/Resources/speech_audios/
 #python run_client_test.py -d -o./hiddenrequest/
 #python run_client_test.py -s -o ./../../Assets/Resources/StaffRespond/
+# python run_client_test.py  -ms -o ./test/
 '''
 usage: run_client_test.py [-h] [-a] [-c] [-d] [-s] [-g] [-t] [-ms] [-o O]
                           [-misc MISC] [-customers CUSTOMERS]
